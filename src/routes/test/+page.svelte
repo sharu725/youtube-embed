89a8e69a2f483946787be8499657e13cef54fe18
sharath <sharath.dt@gmail.com>
2023-05-16 14:04:35 +0530

    update test package

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -5,7 +5,8 @@
 <Youtube id="q2Y3f0lHnMs">
   <img
     slot="thumbnail"
-    src="http://www.hattours.com/blog/wp-content/uploads/2012/07/Hindu-Calendar1.jpg"
+    src="https://picsum.photos/id/1060/536/354?blur=2"
     alt=""
+    style="width:100%; height: 100%; object-fit: cover"
   />
 </Youtube>
