d6d3a00883039fe136b0d34cbcf403db6cb41355
sharath <sharath.dt@gmail.com>
2024-04-04 15:37:03 +0530

    udpate package, add usage block to intro

--- src/routes/Intro.svelte
+++ src/routes/Intro.svelte
@@ -11,10 +11,18 @@
   clicking the play button.
 </p>
 
-
 <h2>Installation</h2>
 <Prism code={`npm i -D svelte-youtube-embed`} />
 
+<h2>Usage</h2>
+<Prism
+  code={`<script>
+  import Youtube from "svelte-youtube-embed";
+</script>
+
+<Youtube id="EBtsu6naB8g" />
+`}
+/>
 
 <h2>Default</h2>
 <Youtube id="EBtsu6naB8g" />
